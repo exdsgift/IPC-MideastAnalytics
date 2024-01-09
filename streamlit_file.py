f71608e5bf5aef995ba096b34f1506ac7c9ae4d8
Gabriele Durante <88145195+exdsgift@users.noreply.github.com>
2024-01-09 11:10:14 +0100

    adding a streamlit file

--- streamlit_file.py
+++ streamlit_file.py
@@ -11,16 +11,6 @@ from wordcloud import WordCloud
 
 fatalities_df = pd.read_csv('/Users/gabrieledurante/Documents/uni/data science UNIVR - Master Degree/programming/datasets _for _final_project/fatalities_isr_pse_conflict_2000_to_2023.csv')
 
-
-
-
-st.text('Fixed width text')
-st.markdown('_Markdown_') # see #*
-st.caption('Balloons. Hundreds of them...')
-st.latex(r''' e^{i\pi} + 1 = 0 ''')
-st.write('Most objects') # df, err, func, keras!
-st.write(['st', 'is <', 3]) # see *
-st.title('My title')
-st.header('My header')
-st.subheader('My sub')
-st.code('for i in range(8): foo()')
+st.title('Exploring Terrorism Victim Data: A Look into the Israeli-Palestinian Conflict')
+st.subheader('Programming and Database Course Final Project')
+st.write('Author: Gabriele Durante')
